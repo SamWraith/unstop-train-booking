8d362090aae2923a35422d8dbdfd382ff335853a
SamWraith <samirnandan2003@gmail.com>
2024-09-26 18:02:07 +0530

    Add route for root endpoint

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({ message: "Hello World" });
+})
 
 // ! import routes
 import seatRoutes from "./routes/seat.routes.js";
